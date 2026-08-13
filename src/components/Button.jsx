76ce3524753371e982b27a4ceee109d47031180a
izzatulloheshimboyev09 <izzatulloheshimboyev09@gmail.com>
2026-08-13 14:02:41 +0500

    colors chnged

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Button({ variant, text, onClick }) {
   let varians = {
     primary:
-      "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
+      "bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded",
     secondary:
-      "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded",
+      "bg-gray-500 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded",
     danger:
-      "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
+      "bg-red-500 hover:bg-red-700 text-white font-medium py-2 px-4 rounded",
   };
   return (
     <button className={varians[variant]} onClick={onClick}>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -14,7 +14,7 @@ function Table() {
   return (
     <div className="p-4 overflow-x-auto">
       <table className="w-full text-left table-auto border-collapse border border-slate-300 shadow-sm rounded-lg overflow-hidden">
-        <thead className="bg-slate-800 text-white uppercase text-xs tracking-wider">
+        <thead className="bg-blue-700 text-white uppercase text-xs tracking-wider">
           <tr>
             <th className="py-3 px-4">ID</th>
             <th className="py-3 px-4">Rasm</th>
